c093e5df70b244fea6759a39895d3855e4b74db6
yuvaramsingh94 <yuvaramsingh94@gmail.com>
2021-03-13 18:09:38 +0000

    local test submission

--- code/model.py
+++ code/model.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
-import torchvision.models as models
+import torch.nn.functional as F
+from torchvision import transforms
+from utils import Normalize
 
 class Autopool(nn.Module):
     def __init__(
@@ -28,31 +30,52 @@ class Autopool(nn.Module):
         return final_out, sigmoid_output
 
 class HpaSub(nn.Module):
-    def __init__(self, classes):
+    def __init__(self, classes, features):
         super(HpaSub, self).__init__()
         self.species = nn.Sequential(
-            nn.Linear(512, 512),
+            nn.Linear(features, 512),
             nn.ReLU(),
             nn.Dropout(p=0.2),
             nn.Linear(512, classes),
         )
 
     def forward(self, GAP):
+        #print('GAP ',GAP.shape)
+        GAP = F.avg_pool2d(GAP, GAP.size()[2:]).squeeze()
+        #rint('GAP ',GAP.shape)
         spe = self.species(GAP)
         return spe
 
 class HpaModel(nn.Module):
-    def __init__(self, classes, device):
+    def __init__(self, classes, device, base_model_name, pretrained, features):
         super(HpaModel, self).__init__()
-        self.model = models.resnet18(pretrained=True)
-        del self.model.fc
-        self.model.fc = HpaSub(classes)
+
+        mean_list = [0.083170892049318, 0.08627143702844145, 0.05734662013795027, 0.06582942296076659]
+        std_list = [0.13561066140407024, 0.13301454127989584, 0.09142918497144226, 0.15651865713966945]
+        self.transform=transforms.Compose([Normalize(mean= mean_list,
+                              std= std_list,
+                              device = device)])
+
+        base_model = torch.hub.load('pytorch/vision', base_model_name, pretrained=pretrained)
+        #print(base_model)
+        #print('the list ',list(base_model.children()))
+        layers = list(base_model.children())[:-1]
+
+
+        self.init_layer = nn.Conv2d(in_channels=4, out_channels=3, kernel_size=1, stride=1)
+
+
+        self.model = nn.Sequential(*layers)
+        self.fc = HpaSub(classes, features)
         self.autopool = Autopool(input_size = classes, device = device)
 
     def forward(self, x):
         batch_size, cells, C, H, W = x.size()
-        c_in = x.view(batch_size * cells, C, H, W)
-        spe = self.model(c_in)
-        spe = spe.view(batch_size, cells, -1)
+        c_in = self.transform(x.view(batch_size * cells, C, H, W))
+        #print('input c_in ',c_in.shape)
+        c_in = F.relu(self.init_layer(c_in))
+        #print('init layer c_in ',c_in.shape)
+        spe = self.fc(self.model(c_in))
+        spe = spe.contiguous().view(batch_size, cells, -1)
         final_output, sigmoid_output = self.autopool(spe)
-        return final_output, sigmoid_output
+        return {'final_output':final_output, 'sigmoid_output':sigmoid_output}
--- code/utils.py
+++ code/utils.py
@@ -11,6 +11,36 @@ def set_seed(seed: int = 42):
     np.random.seed(seed)
     os.environ["PYTHONHASHSEED"] = str(seed)
 
+class Normalize:
+    """Applies the :class:`~torchvision.transforms.Normalize` transform to a batch of images.
+    .. note::
+        This transform acts out of place by default, i.e., it does not mutate the input tensor.
+    Args:
+        mean (sequence): Sequence of means for each channel.
+        std (sequence): Sequence of standard deviations for each channel.
+        inplace(bool,optional): Bool to make this operation in-place.
+        dtype (torch.dtype,optional): The data type of tensors to which the transform will be applied.
+        device (torch.device,optional): The device of tensors to which the transform will be applied.
+    """
+
+    def __init__(self, mean, std, inplace=False, dtype=torch.float, device='cpu'):
+        self.mean = torch.as_tensor(mean, dtype=dtype, device=device)[None, :, None, None]
+        self.std = torch.as_tensor(std, dtype=dtype, device=device)[None, :, None, None]
+        self.inplace = inplace
+        
+    def __call__(self, tensor):
+        """
+        Args:
+            tensor (Tensor): Tensor of size (N, C, H, W) to be normalized.
+        Returns:
+            Tensor: Normalized Tensor.
+        """
+        if not self.inplace:
+            tensor = tensor.clone()
+
+        tensor.sub_(self.mean).div_(self.std)
+        return tensor
+
 class hpa_dataset(data.Dataset):
     def __init__(self, main_df, augmentation = None, path=None,  aug_per = 0.0, cells_used = 8):
         self.main_df = main_df
