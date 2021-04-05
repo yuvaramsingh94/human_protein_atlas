064c6fbfdb9640976f7f9d16442b46cee4b828f5
yuvaramsingh94 <yuvaramsingh94@gmail.com>
2021-04-05 14:14:31 +0000

    test data

--- code/testdata_process.py
+++ code/testdata_process.py
@@ -208,7 +208,7 @@ BATCH_SIZE = 64
 WORKERS = 15
 n_classes = 19
 metric_use = 'loss'
-vees = 'v6_1'
+vees = 'v6_2'
 WORK_LOCATION = f'data/submissions/test_{vees}_{metric_use}/'
 
 
@@ -222,21 +222,21 @@ MODEL_PATH = f'weights/version_{vees}'
 n_classes = 19
 # config_v1.ini
 model_fold_0 = HpaModel(classes = n_classes, device = device, 
-                        base_model_name = 'efficientnet-b4', features = 1792, pretrained = False)
+                        base_model_name = 'resnest50', features = 2048, pretrained = False)
 
-model_fold_0.load_state_dict(torch.load(f"{MODEL_PATH}/fold_{0}_seed_2/model_{metric_use}_{0}.pth",map_location = device))
+model_fold_0.load_state_dict(torch.load(f"{MODEL_PATH}/fold_{0}_seed_1/model_{metric_use}_{0}.pth",map_location = device))
 model_fold_0.to(device)
 model_fold_0.eval()
 
 model_fold_1 = HpaModel(classes = n_classes, device = device, 
-                        base_model_name = 'efficientnet-b4', features = 1792, pretrained = False)
+                        base_model_name = 'resnest50', features = 2048, pretrained = False)
 
-model_fold_1.load_state_dict(torch.load(f"{MODEL_PATH}/fold_{1}_seed_1/model_{metric_use}_{1}.pth",map_location = device))
+model_fold_1.load_state_dict(torch.load(f"{MODEL_PATH}/fold_{1}_seed_2/model_{metric_use}_{1}.pth",map_location = device))
 model_fold_1.to(device)
 model_fold_1.eval()
 
 model_fold_2 = HpaModel(classes = n_classes, device = device, 
-                        base_model_name = 'efficientnet-b4', features = 1792, pretrained = False)
+                        base_model_name = 'resnest50', features = 2048, pretrained = False)
 
 model_fold_2.load_state_dict(torch.load(f"{MODEL_PATH}/fold_{2}_seed_2/model_{metric_use}_{2}.pth",map_location = device))
 model_fold_2.to(device)
