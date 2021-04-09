6493cc87f3814c39797551d3c1ba5d3dc843a700
yuvaramsingh94 <yuvaramsingh94@gmail.com>
2021-04-09 17:36:07 +0000

    hi

--- code/testdata_process.py
+++ code/testdata_process.py
@@ -208,11 +208,8 @@ BATCH_SIZE = 64
 WORKERS = 15
 n_classes = 19
 metric_use = 'loss'
-vees = 'v6_4'
+vees = 'v6_2_1'
 WORK_LOCATION = f'data/submissions/test_{vees}_{metric_use}/'
-
-
-
 
 if not os.path.exists(WORK_LOCATION):
         os.mkdir(WORK_LOCATION)
@@ -222,23 +219,23 @@ MODEL_PATH = f'weights/version_{vees}'
 n_classes = 19
 # config_v1.ini
 model_fold_0 = HpaModel(classes = n_classes, device = device, 
-                        base_model_name = 'efficientnet-b7', features = 2560, pretrained = False)
+                        base_model_name = 'resnest50', features = 2048, pretrained = False)
 
-model_fold_0.load_state_dict(torch.load(f"{MODEL_PATH}/fold_{0}_seed_2/model_{metric_use}_{0}.pth",map_location = device))
+model_fold_0.load_state_dict(torch.load(f"{MODEL_PATH}/st_fold_{0}_seed_1/model_st_{metric_use}_{0}.pth",map_location = device))
 model_fold_0.to(device)
 model_fold_0.eval()
 
 model_fold_1 = HpaModel(classes = n_classes, device = device, 
-                        base_model_name = 'efficientnet-b7', features = 2560, pretrained = False)
+                        base_model_name = 'resnest50', features = 2048, pretrained = False)
 
-model_fold_1.load_state_dict(torch.load(f"{MODEL_PATH}/fold_{1}_seed_1/model_{metric_use}_{1}.pth",map_location = device))
+model_fold_1.load_state_dict(torch.load(f"{MODEL_PATH}/st_fold_{1}_seed_2/model_st_{metric_use}_{1}.pth",map_location = device))
 model_fold_1.to(device)
 model_fold_1.eval()
 
 model_fold_2 = HpaModel(classes = n_classes, device = device, 
-                        base_model_name = 'efficientnet-b7', features = 2560, pretrained = False)
+                        base_model_name = 'resnest50', features = 2048, pretrained = False)
 
-model_fold_2.load_state_dict(torch.load(f"{MODEL_PATH}/fold_{2}_seed_2/model_{metric_use}_{2}.pth",map_location = device))
+model_fold_2.load_state_dict(torch.load(f"{MODEL_PATH}/st_fold_{2}_seed_1/model_st_{metric_use}_{2}.pth",map_location = device))
 model_fold_2.to(device)
 model_fold_2.eval()
 
