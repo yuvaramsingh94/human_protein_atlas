cb204fee11cfdb9631439cd1c13c6d78ccb20717
yuvaramsingh94 <yuvaramsingh94@gmail.com>
2021-04-25 11:35:20 +0000

    removed nucli tourching border from trainig

--- code/image_patch.py
+++ code/image_patch.py
@@ -12,7 +12,9 @@ import cv2
 train_df = pd.read_csv('data/train.csv')
 
 AREA = 40000
-version = 'v5'
+version = 'v6'
+SIZE = 256
+BUFFER = 20#pix
 
 def img_splitter(im_tok):
     #img_filename = img_token+'.png'
@@ -25,8 +27,7 @@ def img_splitter(im_tok):
     image = np.concatenate([img_red, img_yellow, img_green, img_blue], axis=-1)
     img_mask = np.load(os.path.join('data/hpa_cell_mask',f'{im_tok}.npz'))['arr_0'].astype(np.uint8)
     img_nu_mask = np.load(os.path.join('data/hpa_nuclei_mask',f'{im_tok}.npz'))['arr_0'].astype(np.uint8)
-    #print(image.shape,img_mask.shape)
-    #area_list = []
+    Shape = img_mask.shape
     crop_img_list = []
     count = 0
     for i in range(1, img_mask.max() + 1):
@@ -37,6 +38,10 @@ def img_splitter(im_tok):
         try:
             top_nu_left = true_nu_points.min(axis=0)
             bottom_nu_right = true_nu_points.max(axis=0)
+
+            #height , width
+            y1, x1 = top_nu_left
+            y2, x2 = bottom_nu_right
 
             bmask = np.expand_dims(bmask, axis = -1)
             bmask = np.concatenate([bmask, bmask, bmask, bmask], axis=-1)
@@ -49,45 +54,46 @@ def img_splitter(im_tok):
             #print(cropped_arr.shape)
             #print('Area: ',cropped_arr.shape[0] * cropped_arr.shape[1])
             if cropped_arr.shape[0] * cropped_arr.shape[1] > AREA:
-                count += 1
 
-                #lets calcualte teh green channel stats
-                non_zero_count = np.count_nonzero(cropped_arr[:,:,2])
-                relative_freq = np.array(non_zero_count/(cropped_arr.shape[0] * cropped_arr.shape[1]))
-                #print('relative freq ', relative_freq)
-                actual_h = cropped_arr.shape[0]
-                actual_w = cropped_arr.shape[1]
-                cropped_arr = cv2.resize(cropped_arr, (256, 256))
-                #print('after resize ',cropped_arr.min(),cropped_arr.max())
-                if not os.path.exists(f"data/train_h5_256_{AREA}_{version}/{im_tok}"):
-                    os.mkdir(f"data/train_h5_256_{AREA}_{version}/{im_tok}")
+                if not (y1 < BUFFER or x1 < BUFFER or y2 > Shape[0] - BUFFER or x2 > Shape[1] - BUFFER):
+                    count += 1
 
-                hdf5_path = os.path.join(f"data/train_h5_256_{AREA}_{version}/{im_tok}",f'{im_tok}_{count}.hdf5')
-                hdf5_file = h5py.File(hdf5_path, mode='w')
-                hdf5_file.create_dataset("train_img",cropped_arr.shape,np.uint8)
-                hdf5_file.create_dataset("protein_rf",relative_freq.shape,np.float)
-                #hdf5_file.create_dataset("actual_h",relative_freq.shape,np.int)
-                #hdf5_file.create_dataset("actual_w",relative_freq.shape,np.int)
-                hdf5_file["train_img"][...] = cropped_arr
-                hdf5_file["protein_rf"][...] = relative_freq
-                #hdf5_file["actual_h"][...] = actual_h
-                #hdf5_file["actual_w"][...] = actual_w
-                hdf5_file.close()
-                
+                    #lets calcualte teh green channel stats
+                    non_zero_count = np.count_nonzero(cropped_arr[:,:,2])
+                    relative_freq = np.array(non_zero_count/(cropped_arr.shape[0] * cropped_arr.shape[1]))
+                    #print('relative freq ', relative_freq)
+                    actual_h = cropped_arr.shape[0]
+                    actual_w = cropped_arr.shape[1]
+                    cropped_arr = cv2.resize(cropped_arr, (SIZE, SIZE))
+                    #print('after resize ',cropped_arr.min(),cropped_arr.max())
+                    if not os.path.exists(f"data/train_h5_{SIZE}_{AREA}_{version}/{im_tok}"):
+                        #print('creating')
+                        os.mkdir(f"data/train_h5_{SIZE}_{AREA}_{version}/{im_tok}")
 
-                #area_list.append(cropped_arr.shape[0] * cropped_arr.shape[1]
-                #crop_img_list.append(cropped_arr)
-        except:
-            #pass
-            print('no nuce ')
+                    hdf5_path = os.path.join(f"data/train_h5_{SIZE}_{AREA}_{version}/{im_tok}",f'{im_tok}_{count}.hdf5')
+                    hdf5_file = h5py.File(hdf5_path, mode='w')
+                    hdf5_file.create_dataset("train_img",cropped_arr.shape,np.uint8)
+                    hdf5_file.create_dataset("protein_rf",relative_freq.shape,np.float)
+                    hdf5_file["train_img"][...] = cropped_arr
+                    hdf5_file["protein_rf"][...] = relative_freq
+                    hdf5_file.close()
+                #else:
+                    #print('the nuclius is tourching the edge ',im_tok)
+            #else:
+                #print('area ')
+        except :
+            pass
+            #print('no nuce ',im_tok)
             #print(true_nu_points)
 
-img_token_list = ['6057d35c-bb9f-11e8-b2b9-ac1f6b6435d0']#train_df['ID'].values
+
+img_token_list = train_df['ID'].values
 
 #for i in  img_token_list[:3]:
     #print(i)
 #    img_splitter(i)
-
+if not os.path.exists(f"data/train_h5_{SIZE}_{AREA}_{version}"):
+    os.mkdir(f"data/train_h5_{SIZE}_{AREA}_{version}")
 Parallel(n_jobs=20, verbose=5)(# job -1
         delayed(img_splitter)(
             im_tok,
--- code/image_patch_extra.py
+++ code/image_patch_extra.py
@@ -12,8 +12,9 @@ import cv2
 train_df = pd.read_csv('data/train_ext/extra_data_full.csv')
 
 AREA = 40000
-version = 'v5'
+version = 'v6'
 SIZE = 256
+BUFFER = 20#pix
 
 def img_splitter(im_tok):
 
@@ -25,7 +26,7 @@ def img_splitter(im_tok):
     #print(image.max())
     img_mask = np.load(os.path.join('data/train_ext/mask',f'{im_tok}_cell.npz'))['arr_0'].astype(np.uint8)
     img_nu_mask = np.load(os.path.join('data/train_ext/mask',f'{im_tok}_nu.npz'))['arr_0'].astype(np.uint8)
-
+    Shape = img_mask.shape
     crop_img_list = []
     count = 0
     for i in range(1, img_mask.max() + 1):
@@ -36,6 +37,10 @@ def img_splitter(im_tok):
         try:
             top_nu_left = true_nu_points.min(axis=0)
             bottom_nu_right = true_nu_points.max(axis=0)
+
+            #height , width
+            y1, x1 = top_nu_left
+            y2, x2 = bottom_nu_right
 
             bmask = np.expand_dims(bmask, axis = -1)
             bmask = np.concatenate([bmask, bmask, bmask, bmask], axis=-1)
@@ -48,29 +53,36 @@ def img_splitter(im_tok):
             #print(cropped_arr.shape)
             #print('Area: ',cropped_arr.shape[0] * cropped_arr.shape[1])
             if cropped_arr.shape[0] * cropped_arr.shape[1] > AREA:
-                count += 1
 
-                #lets calcualte teh green channel stats
-                non_zero_count = np.count_nonzero(cropped_arr[:,:,2])
-                relative_freq = np.array(non_zero_count/(cropped_arr.shape[0] * cropped_arr.shape[1]))
-                #print('relative freq ', relative_freq)
-                actual_h = cropped_arr.shape[0]
-                actual_w = cropped_arr.shape[1]
-                cropped_arr = cv2.resize(cropped_arr, (SIZE, SIZE))
-                #print('after resize ',cropped_arr.min(),cropped_arr.max())
-                if not os.path.exists(f"data/train_h5_{SIZE}_{AREA}_{version}/{im_tok}"):
-                    os.mkdir(f"data/train_h5_{SIZE}_{AREA}_{version}/{im_tok}")
+                if not (y1 < BUFFER or x1 < BUFFER or y2 > Shape[0] - BUFFER or x2 > Shape[1] - BUFFER):
+                    count += 1
 
-                hdf5_path = os.path.join(f"data/train_h5_{SIZE}_{AREA}_{version}/{im_tok}",f'{im_tok}_{count}.hdf5')
-                hdf5_file = h5py.File(hdf5_path, mode='w')
-                hdf5_file.create_dataset("train_img",cropped_arr.shape,np.uint8)
-                hdf5_file.create_dataset("protein_rf",relative_freq.shape,np.float)
-                hdf5_file["train_img"][...] = cropped_arr
-                hdf5_file["protein_rf"][...] = relative_freq
-                hdf5_file.close()
-        except:
+                    #lets calcualte teh green channel stats
+                    non_zero_count = np.count_nonzero(cropped_arr[:,:,2])
+                    relative_freq = np.array(non_zero_count/(cropped_arr.shape[0] * cropped_arr.shape[1]))
+                    #print('relative freq ', relative_freq)
+                    actual_h = cropped_arr.shape[0]
+                    actual_w = cropped_arr.shape[1]
+                    cropped_arr = cv2.resize(cropped_arr, (SIZE, SIZE))
+                    #print('after resize ',cropped_arr.min(),cropped_arr.max())
+                    if not os.path.exists(f"data/train_h5_{SIZE}_{AREA}_{version}/{im_tok}"):
+                        #print('creating')
+                        os.mkdir(f"data/train_h5_{SIZE}_{AREA}_{version}/{im_tok}")
+
+                    hdf5_path = os.path.join(f"data/train_h5_{SIZE}_{AREA}_{version}/{im_tok}",f'{im_tok}_{count}.hdf5')
+                    hdf5_file = h5py.File(hdf5_path, mode='w')
+                    hdf5_file.create_dataset("train_img",cropped_arr.shape,np.uint8)
+                    hdf5_file.create_dataset("protein_rf",relative_freq.shape,np.float)
+                    hdf5_file["train_img"][...] = cropped_arr
+                    hdf5_file["protein_rf"][...] = relative_freq
+                    hdf5_file.close()
+                #else:
+                #    print('the nuclius is tourching the edge ',im_tok)
+            #else:
+            #    print('area ')
+        except :
             pass
-            #print('no nuce ')
+            #print('no nuce ',im_tok)
             #print(true_nu_points)
 
 img_token_list = train_df['ID'].values
@@ -78,10 +90,16 @@ img_token_list = train_df['ID'].values
 #for i in  img_token_list[:3]:
     #print(i)
 #    img_splitter(i)
-
-Parallel(n_jobs=40, verbose=5)(# job -1
+if not os.path.exists(f"data/train_h5_{SIZE}_{AREA}_{version}"):
+    os.mkdir(f"data/train_h5_{SIZE}_{AREA}_{version}")
+Parallel(n_jobs=10, verbose=5)(# job -1
         delayed(img_splitter)(
             im_tok,
         )
-        for im_tok in img_token_list#[:5]
+        for im_tok in img_token_list#[:20]
     )
+
+'''
+v6 = we are going to remove nuclieus that are tourching the edge of the image,
+
+'''
